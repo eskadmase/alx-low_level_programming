c5235381a51872fd723e13654ee71eeee82275a1
eskadmase <eskadmase.mellion721@gmail.com>
2022-04-09 14:38:04 -0700

    edit for stderr 2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,5 +17,5 @@ printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-fprintf(stderr, "size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+fprintf(stderr, "\n");
 }
