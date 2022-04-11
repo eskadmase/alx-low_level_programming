96b0fddc5deb6b4048890ada98b01ae8834cb1eb
eskadmase <eskadmase.mellion721@gmail.com>
2022-04-11 15:45:35 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print all combinations of single digit
+ * starting from 0, followed by anew line.
+ * Return: 0
+ */
+int main(void)
+{
+int c = 0;
+
+while (c < 10)
+{
+putchar(48 + c);
+if (c != 9)
+{
+putchar(',');
+putchar(' ');
+}
+c++;
+}
+putchar('\n');
+return (0);
+}
