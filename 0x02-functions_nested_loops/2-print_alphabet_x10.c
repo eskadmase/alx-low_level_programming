f984635dc294dbbefff546166193f5287e29a462
eskadmase <eskadmase.mellion721@gmail.com>
2022-04-17 02:48:34 -0700

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the the alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+int counter = 0;
+char letter;
+while (counter++ <= 9)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_printchar(letter);
+_printchar('\n');
+}
+}
