eaa607cdeca716a00c23be7d68e8832c0c15b1de
eskadmase <eskadmase.mellion721@gmail.com>
2022-04-17 12:43:33 -0700

    correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "stdio.h"
 
 /**
  * print_to_98 - prints all natural numbers froma input to 98
@@ -13,6 +13,7 @@ while (n > 98)
 printf("%d, ", n--);
 printf("%d\n", n);
 }
+
 else
 {
 while (n < 98)
