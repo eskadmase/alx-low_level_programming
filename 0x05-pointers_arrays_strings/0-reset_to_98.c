cea0c23ba92a24a1d4429c936501db5a2886c542
eskadmase <eskadmase.mellion721@gmail.com>
2022-04-18 14:16:54 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value to 98
+ * @n: the pointer that will be updated
+ *
+ * Return: void that means is correct
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x05-pointers_arrays_strings is my first pointer and array project
