8ddff597742cb1bd15672de243c3d3e17896ad4e
eskadmase <eskadmase.mellion721@gmail.com>
2022-04-18 16:30:04 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - prints every other character of a string
+ * @str: a pointer to be updated
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int i, last;
+int i = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+last = (i + 1) / 2;
+
+for (i = last; str[i]; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
