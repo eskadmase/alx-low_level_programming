a35b9ec2f60148ae9b634b58cb2806c8b0c4344c
eskadmase <eskadmase.mellion721@gmail.com>
2022-04-19 09:31:35 -0700

    correction for 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,17 +10,17 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && a > c)
+if (a > b && b > c)
 {
 largest = a;
 }
-else if (a > b && c > a)
+else if (b > a && a > c)
 {
-largest = c;
+largest = b;
 }
 else
 {
-largest = b;
+largest = c;
 }
 return (largest);
 }
