39aa79eb5d86f47faea10df23214d84bdfecf716
eskadmase <eskadmase.mellion721@gmail.com>
2022-04-19 01:30:16 -0700

    2-largset_number

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * largest number - return the largest number of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: the largest of all
+ */
+int largest_number(int a, int b, int c)
+{
+int largest;
+
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (a > b && c > a)
+{
+largest = c;
+}
+else if (b > a)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
+return (largest);
+}
