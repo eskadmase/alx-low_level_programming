dbb597750e69ad63c81371149d0a3cb880637311
eskadmase <eskadmase.mellion721@gmail.com>
2022-04-22 19:35:21 -0700

    write function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,16 +11,14 @@
 
 char *_stract(char *dest, char *src)
 {
-int len = strlen(dest), i, n = strlen(src);
-
-for (i = 0; i <= n /* && *src != '\0' */; i++)
-{
-/* printf("%d,\n", i); */
-dest[len + i] = src[i];
-/* src++; */
-/* dest++; */
-}
-/* prinf("i = %d", i); */
-/* dest[len + i] = '\0'; */
+int c, c2;
+c = 0;
+/*find the size of dest array*/
+while (dest[c])
+c++;
+/* iterate through each src array value without the null byte*/
+for (c2 = 0; src[c2] ; c2++)
+/*append src[c2] to dest[c] while while overwritting the null byte in dest*/
+dest[c++] = src[c2];
 return (dest);
 }
