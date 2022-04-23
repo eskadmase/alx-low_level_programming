a881ec09b20a0d156208861aaa5e97f59ee88961
eskadmase <eskadmase.mellion721@gmail.com>
2022-04-23 11:49:51 -0700

    correction 3

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - multiplies two integer
- * @a: first integer
- * @b: second imteger
- * Return: multiplication
+ * main - multiplies two integer.
+ * @a: first integer.
+ * @b: second imteger.
+ * Return: multiplication.
  */
 int mul(int a, int b)
 {
