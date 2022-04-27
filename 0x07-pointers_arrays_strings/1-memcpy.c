320f97a3503f9687ceb647f01da8f368323594f2
eskadmase <eskadmase.mellion721@gmail.com>
2022-04-27 11:11:38 -0700

    correction 2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,15 +10,9 @@
  */
 char *_memcpy(char *dset, char *src, unsigned int n)
 {
-int i = 0;
-int j = 0;
-
-while (n > 0)
+for (i = 0; i < n; i++)
 {
-dest[i] = src[j]
-i++;
-j++;
-n--;
+dest[i] = src [i];
 }
 return (dest);
 }
