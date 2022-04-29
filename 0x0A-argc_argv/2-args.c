d877261a4771c21dd4e66a80978d331ce31717b7
eskadmase <eskadmase.mellion721@gmail.com>
2022-04-29 16:52:16 -0700

    correction

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a function that prints all the arguments it recieves
+ * @argc: number of arguments
+ * @argv: array with size of argc
+ *
+ * Return: 0
+ */
