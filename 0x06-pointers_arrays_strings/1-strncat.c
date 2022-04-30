86f9b7a7fbafde0a52d3515632a61bd6b86b66e9
eskadmase <eskadmase.mellion721@gmail.com>
2022-04-30 15:28:15 -0700

    correction for 1-strncat.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strncat - is that concatenates two strings
  * @dest: pointer to char parameter
@@ -8,10 +9,8 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
-	i = 0;
-	j = 0;
+	int i = 0;
+	int j = 0;
 
 	while (dest[i] != '\0')
 	{
