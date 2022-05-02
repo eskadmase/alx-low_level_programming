d00fea0b2c4db14bc59bb03a09a17a48c340a993
eskadmase <eskadmase.mellion721@gmail.com>
2022-05-02 02:13:46 -0700

    correction

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,6 +7,7 @@
  * @size: integer
  * Return: void
  */
+
 void print_diagsums(int *a, int size)
 {
 	int i = 0, j = size - 1, sum = 0;
@@ -18,7 +19,6 @@ void print_diagsums(int *a, int size)
 	printf("%d, ", sum);
 
 	sum = 0;
-
 	for (; j < (size * size) - size + 1; j += size - 1)
 	{
 		sum += a[j];
