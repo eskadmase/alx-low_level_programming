cb98c6130118e4dd1cb332fdfd4abe1c141f2c70
eskadmase <eskadmase.mellion721@gmail.com>
2022-05-24 10:41:46 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create an array of chars
+ * @size: the size of the memory to print
+ * @c: character to initialize
+ *
+ * Return: pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size <= 0)
+		return (0);
+
+	s = malloc(sizeof(char) * size);
+
+	if (size == 0)
+		return (0);
+
+	for (i = 0; i < size; i++)
+		*(s + i) = c;
+
+	*(s + i) = '\0';
+
+	return (s);
+
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+0X0B-malloc_free is for learning the built in functions malloc and free for proper use of memory
