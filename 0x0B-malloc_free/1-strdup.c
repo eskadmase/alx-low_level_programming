8013f45a6632c3a60457070fcca01f29b8e18461
eskadmase <eskadmase.mellion721@gmail.com>
2022-05-25 01:16:21 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strdup - returns to a pointer to a newly allocated memory
+ * @str: the string to copy
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	char *new_str;
+	int i, j;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{}
+	new_str = malloc(sizeof(char) * (i + 1));
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; *(str + j) != '\0'; j++)
+	{
+		*(new_str + j) = *(str + j);
+	}
+	return (new_str);
+
+}
