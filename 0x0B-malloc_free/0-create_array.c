caa928d30c0c16516a9edf0d477b4854e3ba3772
eskadmase <eskadmase.mellion721@gmail.com>
2022-05-24 23:36:08 -0700

    correction

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,18 +15,22 @@ char *create_array(unsigned int size, char c)
 	char *s;
 
 	if (size == 0)
+	{
 		return (0);
+	}
 
 	s = malloc(sizeof(char) * size);
 
+
 	if (size == 0)
+	{
 		return (0);
+	}
 
 	for (i = 0; i < size; i++)
+	{
 		*(s + i) = c;
-
-	*(s + i) = '\0';
+	}
 
 	return (s);
-
 }
