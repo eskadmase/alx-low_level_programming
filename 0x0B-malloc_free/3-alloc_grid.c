52cf9d05bb1f1d6f1b3b3133ba884ec27bc99616
eskadmase <eskadmase.mellion721@gmail.com>
2022-05-26 09:08:30 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,14 +22,14 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	
+
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
 
 		if (grid[i] == NULL)
 		{
-			for(; i >= 0; i--)
+			for (; i >= 0; i--)
 			{
 				free(grid[i]);
 			}
