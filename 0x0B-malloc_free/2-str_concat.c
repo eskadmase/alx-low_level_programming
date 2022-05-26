7ff5b1a6eaedb8f97bd1e77edf209cf4bf348430
eskadmase <eskadmase.mellion721@gmail.com>
2022-05-26 01:33:05 -0700

    correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,28 +9,27 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s;
-	int i = 0, j = 0, k = 0;
-	if (s1 != NULL)
+	unsigned int i = 0, j = 0, k, l;
+	int size;
+	char *ptr;
 
-	for (i = 0; s1[i]; i++)
-	if (s2 != NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (*(s1 + 1) != '\0')
+		i++;
+	while (*(s2 + j) != '\0')
+		j++;
+	size = i + j;
+	ptr = malloc(sizeof(char) * (size + 1));
 
-	for (j = 0; s2[j]; j++)
+	if (ptr == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+		*(ptr + k) = *(s1 + k);
+	for (l = 0; l < j; l++)
+		*(ptr + (k + l)) = *(s2 + l);
+	return (ptr);
 
-	s = malloc(sizeof(char) * (i + j + 1));
-
-	if (s == NULL)
-	while (k < i)
-	{
-		s[k] = s1 [k];
-		k++;
-	}
-	while (k < i +j)
-	{
-		s[k] = s2[k -i];
-		k++;
-	}
-	s[k] = '\0';
-	return (s);
 }
